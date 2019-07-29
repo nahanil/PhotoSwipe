301fc1e83e4b6af1549271c33d415deed6c29be8
Jarrod Linahan <jarrod@linahan.id.au>
2019-07-30 05:39:07 +1000

    Fix getAttribute errors - dimsemenov/PhotoSwipe/pull/1371

--- src/js/ui/photoswipe-ui-default.js
+++ src/js/ui/photoswipe-ui-default.js
@@ -572,6 +572,7 @@ var PhotoSwipeUI_Default =
 			var t = e.target || e.srcElement;
 			if(
 				t && 
+        t.getAttribute &&
 				t.getAttribute('class') && e.type.indexOf('mouse') > -1 && 
 				( t.getAttribute('class').indexOf('__caption') > 0 || (/(SMALL|STRONG|EM)/i).test(t.tagName) ) 
 			) {
